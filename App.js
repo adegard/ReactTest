cb6155f7dda60f793507d6e6cf13f8f6f867733e
adegard <degardinarnaud@gmail.com>
2022-05-14 17:45:42 +0200

    okk

--- App.js
+++ App.js
@@ -2,14 +2,9 @@ import react from "react"
 
 function App(){
 
-        function afterClick(e){
-           console.log(e.target.value)
-        }
-
-
      return  <>
                 <p>some text</p>
-                <input type="text" onChange={afterClick} />
+               
             </>
 }
 
