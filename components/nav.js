413bcfedd77f80edcf10ccac6f67a0b16b54c71e
adegard <degardinarnaud@gmail.com>
2022-05-15 12:12:28 +0200

    working

--- components/nav.js
+++ components/nav.js
@@ -1,18 +1,22 @@
 import react from "react"
-import About from "./about"
-import Errormsg from "./error404"
 import { Link } from "react-router-dom";
-import App from "../App";
 
 function Nav(){
 
      return  <>
-                <nav >
-                    <Link to="/">{App}</Link> 
-                    <Link to="/about">{About}</Link>
-                    <Link to="*">{Errormsg}</Link>
+               <nav >
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link> 
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
                 </nav>
             </>
 }
-
+                    // <Link to="/">{App}</Link> 
+                    // <Link to="/about">{About}</Link>
+                    // <Link to="*">{Errormsg}</Link>
 export default Nav
